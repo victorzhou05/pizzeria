7cfa9091d60444e536b4227fb28f2879aafc17ef
Victor Zhou <9870482@alu.murciaeduca.es>
2025-02-05 09:23:30 +0100

    Commit 5/2

--- Pizzería/Vistas/CrearPizza.cs
+++ Pizzería/Vistas/CrearPizza.cs
@@ -12,9 +12,11 @@ namespace Pizzería.Vistas
 {
     public partial class CrearPizza : Form
     {
-
-        private List<string> ingrBase;
-        private List<string> ingrIngr;
+        private string Masa;
+        private string Tamano;
+        private Dictionary<string, int> ingrBase;
+        private Dictionary<string, int> ingrCarne;
+        private Dictionary<string, int> ingrVerdura;
 
         public CrearPizza()
         {
@@ -25,9 +27,11 @@ namespace Pizzería.Vistas
             gbBase.Visible = true;
             gbIngredientes.Visible = false;
             gbCompletar.Visible = false;
-            ingrBase = new List<string>();
-            ingrIngr = new List<string>();
+            ingrBase = new Dictionary<string, int>();
+            ingrCarne = new Dictionary<string, int>();
+            ingrVerdura = new Dictionary<string, int>();
         }
+
 
         private void btnIngrAtras_Click(object sender, EventArgs e)
         {
@@ -35,77 +39,281 @@ namespace Pizzería.Vistas
             gbBase.Visible = true;
         }
 
+
         private void btnCompAtras_Click(object sender, EventArgs e)
         {
             gbCompletar.Visible = false;
             gbIngredientes.Visible = true;
         }
 
+        
         private void btnIngrSig_Click(object sender, EventArgs e)
         {
-            ingrIngr.Clear();
+            
             lvCarnes.Clear();
             lvVerduras.Clear();
 
-            if (clbCarnes.CheckedItems.Count > 0)
+            if (ingrCarne.Count > 0)
             {
-                for(int i = 0; i<clbCarnes.CheckedItems.Count; i++)
+                
+                foreach (KeyValuePair<string, int> par in ingrCarne)
                 {
-                    ingrIngr.Add(clbCarnes.CheckedItems[i].ToString());
-                    lvCarnes.Items.Add(clbCarnes.CheckedItems[i].ToString());
+                    string ingredientes = par.Key + ": " + par.Value;
+                    lvCarnes.Items.Add(ingredientes);
                 }
             }
 
-            if (clbVerduras.CheckedItems.Count > 0)
+            if (ingrVerdura.Count > 0)
             {
-                for (int i = 0; i < clbVerduras.CheckedItems.Count; i++)
+                foreach (KeyValuePair<string, int> par in ingrVerdura)
                 {
-                    ingrIngr.Add(clbVerduras.CheckedItems[i].ToString());
-                    lvVerduras.Items.Add(clbVerduras.CheckedItems[i].ToString());
+                    string ingredientes = par.Key + ": " + par.Value;
+                    lvVerduras.Items.Add(ingredientes);
                 }
             }
-
 
             gbIngredientes.Visible = false;
             gbCompletar.Visible = true;
-
+            
         }
 
         private void btnMasaSig_Click(object sender, EventArgs e)
         {
-      
-            string masa = cbMasa.Text;
-            string queso = cbQueso.Text;
-            string salsa = cbSalsa.Text;
 
-            if (masa == "" || queso == "" || salsa == "")
+            this.Masa = cbMasa.Text;
+            this.Tamano = cbTamano.Text;
+
+            if (ingrBase.Count <= 0 || Masa == "" || Tamano == "")
             {
-                MessageBox.Show("Seleccione todos los datos");
+                MessageBox.Show("Complete los campos y añada una salsa o un queso");
                 return;
             }
 
-            ingrBase.Clear();
-            
-            ingrBase.Add(masa);
-            ingrBase.Add(queso);
-            ingrBase.Add(salsa);
+
 
             lvBase.Clear();
             lvBase.Items.Add("Tamaño: " + cbTamano.Text);
             lvBase.Items.Add("Masa: " + cbMasa.Text);
-            lvBase.Items.Add("Queso: " + cbQueso.Text);
-            lvBase.Items.Add("Salsa: " + cbSalsa.Text);
+            foreach (KeyValuePair<string, int> par in ingrBase)
+            {
+                string ingredientes = par.Key + ": " + par.Value;
+                lvBase.Items.Add(ingredientes);
+            }
 
-
-            gbBase.Visible = false;
             gbIngredientes.Visible = true;
+            gbBase.Visible = false;
         }
-
         private void btnCompFin_Click(object sender, EventArgs e)
         {
 
             //Insertar los datos en la base de datos
             this.Close();
+        }
+
+        private void ModificarIngrediente(Dictionary<string, int> dic, string ingrediente, bool incrementar, Label label)
+        {
+            
+            if (dic.ContainsKey(ingrediente)){
+                int numero = int.Parse(label.Text);
+                if (incrementar)
+                {
+
+                    dic[ingrediente] += 1;
+                    label.Text = (numero + 1).ToString();
+                }
+                else
+                {
+                    if (dic[ingrediente] > 0)
+                    {
+                        dic[ingrediente] -= 1;
+                        label.Text = (numero - 1).ToString();
+                        if (dic[ingrediente] == 0)
+                        {
+                            dic.Remove(ingrediente);
+                        }
+                    }
+                }
+                return;
+            }
+
+            if (incrementar)
+            {
+                dic.Add(ingrediente, 1);
+                label.Text = "1";
+            }
+        }
+
+
+        //Boton Salsas
+
+        private void btnTomateMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Tomate", false, labelTomate);
+
+        }
+
+        private void btnTomateMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Tomate", true, labelTomate);
+        }
+
+        private void btnCarbonaraMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Carbonara", false, labelCarbonara);
+        }
+
+        private void btnCarbonaraMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Carbonara", true, labelCarbonara);
+        }
+
+        private void btnBarbacoaMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Barbacoa", false, labelBarbacoa);
+        }
+
+        private void btnBarbacoaMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Barbacoa", true, labelBarbacoa);
+        }
+
+        //Boton Quesos
+
+        private void btnMozzarelaMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Mozzarela", false, labelMozzarela);
+        }
+
+        private void btnMozzarelaMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Mozzarela", true, labelMozzarela);
+        }
+
+        private void btnCheddarMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Cheddar", false, labelCheddar);
+        }
+
+        private void btnCheddarMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Cheddar", true, labelCheddar);
+        }
+
+        private void btnProvoloneMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Provolone", false, labelProvolone);
+        }
+
+        private void btnProvoloneMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Provolone", true, labelProvolone);
+        }
+
+        private void btnSuizoMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrBase, "Suizo", false, labelSuizo);
+        }
+
+        private void btnSuizoMas_Click(object sender, EventArgs e) { 
+        
+            ModificarIngrediente(ingrBase, "Suizo", true, labelSuizo);
+        }
+
+        //Botones Carne
+        private void btnTerneraMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Ternera", false, labelTernera);
+        }
+
+        private void btnTerneraMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Ternera", true, labelTernera);
+        }
+        private void btnPolloMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Pollo", false, labelPollo);
+        }
+
+        private void btnPolloMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Pollo", true, labelPollo);
+        }
+
+        private void btnPepperoniMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Pepperoni", false, labelPepperoni);
+        }
+
+        private void btnPepperoniMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Pepperoni", true, labelPepperoni);
+        }
+
+        private void btnCerdoMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Cerdo", false, labelCerdo);
+        }
+
+        private void btnCerdoMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Cerdo", true, labelCerdo);
+        }
+
+        private void btnAtunMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Atun", false, labelAtun);
+        }
+
+        private void btnAtunMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrCarne, "Atun", true, labelAtun);
+        }
+
+        //Verduras
+
+        private void btnChampinonesMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Champiñones", false, labelChampinon);
+        }
+
+        private void btnChampinonesMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Champiñones", true, labelChampinon);
+        }
+
+        private void btnPimientoMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Pimiento", false, labelPimiento);
+        }
+
+        private void btnPimientoMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Pimiento", true, labelPimiento);
+        }
+
+        private void btnCebollaMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Cebolla", false, labelCebolla);
+        }
+
+        private void btnCebollaMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Cebolla", true, labelCebolla);
+        }
+
+        private void btnAceitunasMenos_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Aceitunas", false, labelAceitunas);
+        }
+
+        private void btnAceitunasMas_Click(object sender, EventArgs e)
+        {
+            ModificarIngrediente(ingrVerdura, "Aceitunas", true, labelAceitunas);
+        }
+
+        private void gbBase_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
